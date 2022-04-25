9d06563f0196a442afaac7c95ab03587ff39b4d8
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-04-25 07:31:52 +0600

    added

---
+++ pages/api/create.ts
@@ -0,0 +1,19 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../lib/prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+	const {title, content} = req.body
+
+	try {
+		await prisma.note.create({
+			data: {
+				title,
+				content
+			}
+		})
+		res.status(200).json({message: 'Note Created'})
+	} catch (error) {
+		console.log("Failured to create note");
+	}
+}
